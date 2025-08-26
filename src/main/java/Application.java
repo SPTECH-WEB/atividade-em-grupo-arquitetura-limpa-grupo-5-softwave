9f8b019f7585ab8a7c6f3a9dbc77814044f294ba
brzenit <bryan.lima@sptech.school>
2025-08-26 19:45:11 -0300

    Estrutura de pastas e inicio do projeto

---
+++ src/main/java/Application.java
@@ -0,0 +1,9 @@
+//@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(BackendSoftWaveApplication.class, args);
+        System.out.println("Hello, World!");
+    }
+
+}
