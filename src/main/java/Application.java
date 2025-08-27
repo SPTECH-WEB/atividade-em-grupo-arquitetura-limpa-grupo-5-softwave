9f5ca9d0a8442060a6fb44fc23832c753d80713c
brzenit <bryan.lima@sptech.school>
2025-08-26 21:07:13 -0300

    spring update

--- src/main/java/Application.java
+++
@@ -1,9 +0,0 @@
-//@SpringBootApplication
-public class Application {
-
-    public static void main(String[] args) {
-//        SpringApplication.run(BackendSoftWaveApplication.class, args);
-        System.out.println("Hello, World!");
-    }
-
-}
